2adb0fc02891d05a2b08145f512d79d6c2b4cc62
Anton Evdokimov <free0u@gmail.com>
2013-04-09 23:08:19 +0400

    Add algo "segment intersect segment" with tests and vis

--- include/cg/operations/has_intersection/segment_segment.h
+++ include/cg/operations/has_intersection/segment_segment.h
@@ -8,5 +8,27 @@
 
 namespace cg
 {
-   bool has_intersection(segment_2 a, segment_2 b);
+   bool has_intersection(segment_2 a, segment_2 b)
+   {
+      orientation_t t0 = orientation(a[0], a[1], b[0]);
+      orientation_t t1 = orientation(a[0], a[1], b[1]);
+
+      if (t0 == t1 && t0 == CG_COLLINEAR) // segments is collinear
+      {
+         if (a[0] > a[1]) {
+            std::swap(a[0], a[1]);
+         }
+         if (b[0] > b[1]) {
+            std::swap(b[0], b[1]);
+         }
+         range_t<point_2> ra(a[0], a[1]);
+         range_t<point_2> rb(b[0], b[1]);
+         return !((ra & rb).is_empty());
+      }
+
+      orientation_t t2 = orientation(b[0], b[1], a[0]);
+      orientation_t t3 = orientation(b[0], b[1], a[1]);
+
+      return t0 != t1 && t2 != t3;
+   }
 }
--- tests/has_intersection.cpp
+++ tests/has_intersection.cpp
@@ -3,11 +3,11 @@
 #include <cg/operations/has_intersection/segment_segment.h>
 #include <cg/operations/has_intersection/triangle_segment.h>
 
-TEST(has_intersection, segment_segment)
-{
-   using cg::point_2;
-   using cg::segment_2;
+using cg::point_2;
+using cg::segment_2;
 
+TEST(has_intersection, segment_segment0)
+{
    segment_2 s[] =
    {
       segment_2(point_2(0, 0), point_2(2, 2)),
@@ -23,15 +23,51 @@ TEST(has_intersection, segment_segment)
    EXPECT_FALSE(cg::has_intersection(s[1], s[3]));
 }
 
-TEST(has_intersection, triangle_segment)
+TEST(has_intersection, segment_segment1)
 {
-   using cg::point_2;
-   using cg::segment_2;
+   segment_2 a(point_2(1, 1), point_2(2, 2));
+   segment_2 b(point_2(2, 2), point_2(3, 2));
 
-   cg::triangle_2 t(point_2(0, 0), point_2(1, 1), point_2(2, 0));
+   EXPECT_TRUE(cg::has_intersection(a, b));
+}
 
-   EXPECT_TRUE(cg::has_intersection(t, segment_2(point_2(0, 0), point_2(-1, -1))));
+TEST(has_intersection, segment_segment2)
+{
+    segment_2 a(point_2(1, 1), point_2(20, 20));
+    segment_2 b(point_2(2, 2), point_2(3, 3));
 
-   EXPECT_TRUE(cg::has_intersection(t, segment_2(point_2(0, 1), point_2(2, 1))));
-   EXPECT_FALSE(cg::has_intersection(t, segment_2(point_2(0, 2), point_2(2, 2))));
+    EXPECT_TRUE(cg::has_intersection(a, b));
+}
+
+TEST(has_intersection, segment_segment3)
+{
+    segment_2 a(point_2(1, 1), point_2(2, 2));
+    segment_2 b(point_2(1.5, 1.5), point_2(1.5, 1.5));
+
+    EXPECT_TRUE(cg::has_intersection(a, b));
+}
+
+TEST(has_intersection, segment_segment4)
+{
+    segment_2 a(point_2(1, 1), point_2(2, 2));
+    segment_2 b(point_2(0, 1.5), point_2(10, 1.5));
+
+    EXPECT_TRUE(cg::has_intersection(a, b));
+}
+
+
+TEST(has_intersection, segment_segment5)
+{
+    segment_2 a(point_2(1, 1), point_2(2, 2));
+    segment_2 b(point_2(3, 3), point_2(4, 4));
+
+    EXPECT_FALSE(cg::has_intersection(a, b));
+}
+
+TEST(has_intersection, segment_segment6)
+{
+    segment_2 a(point_2(3, 3), point_2(4, 4));
+    segment_2 b(point_2(1, 1), point_2(2, 2));
+
+    EXPECT_FALSE(cg::has_intersection(a, b));
 }
